3b86557cbeb5110674f475e8b7afd26c8a0ef0f3
eduda <eduda.agc@gmail.com>
2026-04-14 15:11:46 -0300

    pegando partes da aula 13 para o projeto: renderização, dados para gpu, movimento de câmera etc

--- Projeto/main.py
+++ Projeto/main.py
@@ -43,7 +43,7 @@ glEnable(GL_LINE_SMOOTH)
 # carrega modelo abobora
 vi_abobora, quantosV_abobora = load_obj_and_texture("objetos/abobora/abobora.obj", ["objetos/abobora/texturas/..."])
 
-def desenha_abobora(angle, r_x, r_y, r_z, t_x, t_y, t_z, s_x, s_y, s_z, textureId):
+def desenha_objeto(vertice_inicio, quantos_vertices, angle, r_x, r_y, r_z, t_x, t_y, t_z, s_x, s_y, s_z, textureId):
 
     mat_model = model(angle, r_x, r_y, r_z, t_x, t_y, t_z, s_x, s_y, s_z)
     loc_model = glGetUniformLocation(program, "model")
@@ -53,4 +53,171 @@ def desenha_abobora(angle, r_x, r_y, r_z, t_x, t_y, t_z, s_x, s_y, s_z, textureI
     glBindTexture(GL_TEXTURE_2D, textureId)
     
     # desenha o modelo
-    glDrawArrays(GL_TRIANGLES, vi_abobora, quantosV_abobora) ## renderizando
+    glDrawArrays(GL_TRIANGLES, vertice_inicio, quantos_vertices) ## renderizando
+
+buffer_VBO = glGenBuffers(2)
+
+vertices = np.zeros(len(vertices_list), [("position", np.float32, 3)])
+vertices['position'] = vertices_list
+
+
+# Upload data
+glBindBuffer(GL_ARRAY_BUFFER, buffer_VBO[0])
+glBufferData(GL_ARRAY_BUFFER, vertices.nbytes, vertices, GL_STATIC_DRAW)
+stride = vertices.strides[0]
+offset = ctypes.c_void_p(0)
+loc_vertices = glGetAttribLocation(program, "position")
+glEnableVertexAttribArray(loc_vertices)
+glVertexAttribPointer(loc_vertices, 3, GL_FLOAT, False, stride, offset)
+
+
+# eventos para modificar a posição da câmera
+#cameraPos   = glm.vec3(0.0,  0.0,  1.0);
+#cameraFront = glm.vec3(0.0,  0.0, -1.0);
+#cameraUp    = glm.vec3(0.0,  1.0,  0.0);
+
+# camera
+cameraPos   = glm.vec3(0.0, 0.0, 3.0)
+cameraFront = glm.vec3(0.0, 0.0, -1.0)
+cameraUp    = glm.vec3(0.0, 1.0, 0.0)
+
+firstMouse = True
+yaw   = -90.0	# yaw is initialized to -90.0 degrees since a yaw of 0.0 results in a direction vector pointing to the right so we initially rotate a bit to the left.
+pitch =  0.0
+lastX =  largura / 2.0
+lastY =  altura / 2.0
+fov   =  45.0
+
+# timing
+deltaTime = 0.0	# time between current frame and last frame
+lastFrame = 0.0
+
+
+firstMouse = True
+yaw = -90.0 
+pitch = 0.0
+lastX =  largura/2
+lastY =  altura/2
+
+
+def key_event(window,key,scancode,action,mods):
+    global cameraPos, cameraFront, cameraUp, polygonal_mode
+
+    if key == glfw.KEY_ESCAPE and action == glfw.PRESS:
+        glfw.set_window_should_close(window, True)
+    
+    cameraSpeed = 50 * deltaTime
+    if key == glfw.KEY_W and (action == glfw.PRESS or action == glfw.REPEAT):
+        cameraPos += cameraSpeed * cameraFront
+    
+    if key == glfw.KEY_S and (action == glfw.PRESS or action == glfw.REPEAT):
+        cameraPos -= cameraSpeed * cameraFront
+    
+    if key == glfw.KEY_A and (action == glfw.PRESS or action == glfw.REPEAT):
+        cameraPos -= glm.normalize(glm.cross(cameraFront, cameraUp)) * cameraSpeed
+        
+    if key == glfw.KEY_D and (action == glfw.PRESS or action == glfw.REPEAT):
+        cameraPos += glm.normalize(glm.cross(cameraFront, cameraUp)) * cameraSpeed
+
+    if key == glfw.KEY_P and action == glfw.PRESS:
+        polygonal_mode = not polygonal_mode
+        
+
+def framebuffer_size_callback(window, largura, altura):
+
+    # make sure the viewport matches the new window dimensions note that width and 
+    # height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, largura, altura)
+
+# glfw: whenever the mouse moves, this callback is called
+# -------------------------------------------------------
+def mouse_callback(window, xpos, ypos):
+    global cameraFront, lastX, lastY, firstMouse, yaw, pitch
+   
+    if (firstMouse):
+
+        lastX = xpos
+        lastY = ypos
+        firstMouse = False
+
+    xoffset = xpos - lastX
+    yoffset = lastY - ypos # reversed since y-coordinates go from bottom to top
+    lastX = xpos
+    lastY = ypos
+
+    sensitivity = 0.1 # change this value to your liking
+    xoffset *= sensitivity
+    yoffset *= sensitivity
+
+    yaw += xoffset
+    pitch += yoffset
+
+    # make sure that when pitch is out of bounds, screen doesn't get flipped
+    if (pitch > 89.0):
+        pitch = 89.0
+    if (pitch < -89.0):
+        pitch = -89.0
+
+    front = glm.vec3()
+    front.x = glm.cos(glm.radians(yaw)) * glm.cos(glm.radians(pitch))
+    front.y = glm.sin(glm.radians(pitch))
+    front.z = glm.sin(glm.radians(yaw)) * glm.cos(glm.radians(pitch))
+    cameraFront = glm.normalize(front)
+
+# glfw: whenever the mouse scroll wheel scrolls, this callback is called
+# ----------------------------------------------------------------------
+def scroll_callback(window, xoffset, yoffset):
+    global fov
+
+    fov -= yoffset
+    if (fov < 1.0):
+        fov = 1.0
+    if (fov > 45.0):
+        fov = 45.0
+    
+glfw.set_key_callback(window,key_event)
+glfw.set_framebuffer_size_callback(window, framebuffer_size_callback)
+glfw.set_cursor_pos_callback(window, mouse_callback)
+glfw.set_scroll_callback(window, scroll_callback)
+
+# tell GLFW to capture our mouse
+glfw.set_input_mode(window, glfw.CURSOR, glfw.CURSOR_DISABLED)
+
+def model(angle, r_x, r_y, r_z, t_x, t_y, t_z, s_x, s_y, s_z):
+    
+    angle = math.radians(angle)
+    
+    matrix_transform = glm.mat4(1.0) # instanciando uma matriz identidade
+       
+    # aplicando translacao (terceira operação a ser executada)
+    matrix_transform = glm.translate(matrix_transform, glm.vec3(t_x, t_y, t_z))    
+    
+    # aplicando rotacao (segunda operação a ser executada)
+    if angle!=0:
+        matrix_transform = glm.rotate(matrix_transform, angle, glm.vec3(r_x, r_y, r_z))
+    
+    # aplicando escala (primeira operação a ser executada)
+    matrix_transform = glm.scale(matrix_transform, glm.vec3(s_x, s_y, s_z))
+    
+    matrix_transform = np.array(matrix_transform)
+    
+    return matrix_transform
+
+def view():
+    global cameraPos, cameraFront, cameraUp
+    mat_view = glm.lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+    mat_view = np.array(mat_view)
+    return mat_view
+
+def projection():
+    global altura, largura
+    # perspective parameters: fovy, aspect, near, far
+    mat_projection = glm.perspective(glm.radians(fov), largura/altura, 0.1, 100.0)
+
+    
+    mat_projection = np.array(mat_projection)    
+    return mat_projection
+
+# exibe a janela 
+# loop principal da janela, mas primeiro vou modularizar o código para ficar mais organizado
+glfw.show_window(window)
